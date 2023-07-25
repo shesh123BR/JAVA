c37e7a8bd475607619c39fd0ee88aa80fdf7d5bc
Felipe <125655705+shesh123BR@users.noreply.github.com>
2023-07-25 08:28:35 -0300

    Add files via upload

---
+++ CALC_MEDIAJS/index2.js
@@ -0,0 +1,20 @@
+Function somar(){
+
+var media=document.getElementbyld("media")
+
+var br1=document.getElementById("barra1")
+
+var br2=document.getElementById("barra2")
+
+var br3=document.getElementById("barra3")
+
+var br1= Number(br1.value)
+
+var br1= Number(br2.value)
+
+var br3= Number(br3.value)
+
+var valor = (br1 * 2 + br2 * 3 + br3 * 5)/10
+
+document.getElementById
+}
---
+++ CALC_MEDIAJS/index3.js
@@ -0,0 +1,26 @@
+function clicou(){
+    
+    var media = document.getElementById("media")
+
+    var b1 = document.getElementById("barra1")
+
+    var b1 = document.getElementById('barra2')
+
+    var media = String(media.String)
+
+    var valor =[600-(b1 * 2+ b2 * 3)]/ 5
+
+    document.getElementById("media").innerHTML = valor
+
+    document.getElementById("nota-necessária").innerHTML="nota necessária"
+
+
+
+
+
+
+
+
+
+
+}
